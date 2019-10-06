0b71063cc856b484beff58ef50364cf1034544f2
Kevin Ortega <korte004@ucr.edu>
2019-10-05 23:55:40 -0700

    Update BLDC_Motors.ino
    initialized SM variables

--- BLDC_Motors.ino
+++ BLDC_Motors.ino
@@ -1,4 +1,23 @@
 #include <Servo.h>
+
+//Motor state declarations
+enum MotorStates {INIT, WAIT, INCREASE, DECREASE, MOTOR_SELECT};
+MotorStates motorstate = INIT; 
+
+//Create ESC objects
+Servo ESC1;
+Servo ESC2;
+Servo ESC3;
+Servo ESC4;
+
+//ESC pins, PWM pins on Arduino Nano
+uint8_t escPin1 = 6;
+uint8_t escPin1 = 9;
+uint8_t escPin1 = 10;
+uint8_t escPin1 = 11;
+
+//Min/Max pulse frequency of PWM 
+
 void setup() {
   // put your setup code here, to run once:
 
