7c984c1f53f1b0c4b7d66f8d39603c8f2d8da07f
Kevin Ortega <korte004@ucr.edu>
2019-10-06 23:07:29 -0700

    Update BLDC_Motors.ino
    Implemented Read User Input from Keyboard over Serial

--- BLDC_Motors.ino
+++ BLDC_Motors.ino
@@ -8,9 +8,11 @@
 #define maxPulseRate    2000  //Max Pulse Width of PWM
 #define minThrottle     0     //Min Value for Throttle (Maps to Min Pulse Width)
 #define maxThrottle     180   //Max value for Throttle (Maps to Max Pulse Width)
+#define throttleDelay   50    //Delay value in millisec when adjusting motor speed
 
-unsigned long chrono = millis();  //timer
-uint8_t throttle;                 //motor throttle value
+unsigned long timer = millis();   //Timer
+uint8_t throttle = 0;                 //Motor throttle value
+uint8_t stepValue = 25;                //Throttle Step Value
 
 /*Motor state declarations*/
 enum MotorStates {INIT, WAIT, INCREASE, DECREASE, MOTOR_SELECT};
@@ -22,11 +24,17 @@ Servo ESC2;
 Servo ESC3;
 Servo ESC4;
 
+/*Initial User Prompt*/
+void prompt();
+
 /*Initializes ESCs*/
 void initEsc();
 
 /*Rev up them fryers*/
 void StartUp();
+
+/*Read User input and verify validity*/
+char readUserInput();
 
 /*Read current value of motor throttle*/
 void readThrottle(uint8_t motor);
@@ -38,18 +46,51 @@ void changeThrottle(uint8_t motor,uint8_t throttleValue);
 void motorSM(uint8_t throttle){
   switch(motorState){
     case INIT: {
+      initEsc();
+      motorState = WAIT;
       break;
     }
     case WAIT:{
+      //Wait for user input
+      //if user input is read, switch to appropriate state
+      if(Serial.available() > 0){
+        if(readUserInput() == 'W'){
+          motorState = INCREASE;  
+        }
+        else if(readUserInput() == 'S'){
+          motorState = DECREASE;  
+        }
+        else if((readUserInput() == 'A')||(readUserInput() == 'D')){
+          motorState = MOTOR_SELECT;  
+        }
+        else if(readUserInput() == 'E'){
+          motorState = WAIT;  
+        }
+      }
       break;
     }
     case INCREASE: {
+      Serial.print("I am in increase state! ");
+      Serial.println();
+      Serial.print("Going to Wait now :)");
+      Serial.println();
+      motorState = WAIT;
       break;
     }
     case DECREASE: {
+      Serial.print("I am in decrease state! ");
+      Serial.println();
+      Serial.print("Going to Wait now :)");
+      Serial.println();
+      motorState = WAIT;
       break;
     }
     case MOTOR_SELECT: {
+      Serial.print("You want to select a motor? pshhh! ");
+      Serial.println();
+      Serial.print("Going to Wait now :)");
+      Serial.println();
+      motorState = WAIT;
       break;
     }
   }
@@ -57,11 +98,23 @@ void motorSM(uint8_t throttle){
 
 void setup() {
   Serial.begin(9600);
-  initEsc();
+  prompt(); 
 }
 
 void loop() {
   motorSM(throttle);
+}
+
+
+void prompt(){
+  Serial.print("Motor Controller:");
+  Serial.println();
+  Serial.print("Press \'W\' to increase speed.");
+  Serial.println();
+  Serial.print("Press \'S\' to decrease speed.");
+  Serial.println();
+  Serial.print("Press \'A\' or \'D\' to select motor.");
+  Serial.println();
 }
 
 void initEsc(){
@@ -70,4 +123,64 @@ void initEsc(){
   ESC3.attach(escPin3, minPulseRate, maxPulseRate);
   ESC4.attach(escPin4, minPulseRate, maxPulseRate);
   return; 
-};
+}
+
+char readUserInput(){
+
+  char key = ' ';
+  int incomingData;
+
+  if(Serial.available() > 0 ) {
+    incomingData = Serial.read();
+  } else {
+    return 'E';  
+  }
+  // for whatever reason fails to read, return E
+  if(incomingData == -1) {
+    Serial.print("failed");
+    return 'E';
+  }
+  
+  Serial.print(incomingData);
+  Serial.println();
+
+  switch(incomingData){
+    case 119 : {
+      key = 'W';
+      break;
+    }
+
+    case 97 : {
+      key = 'A';
+      break;
+    }
+  
+    case 115 : {
+      key = 'S';
+      break;  
+    }
+ 
+    case 100 :  {
+      key = 'D';
+      break;
+    }
+
+    default : {
+      key = 'E';
+      Serial.print("Error: Please enter valid key");
+      Serial.print(incomingData);
+      Serial.println();  
+      break;
+    }
+  }
+  return key;
+}
+
+void readThrottle(){ 
+}
+
+void Sflush(){
+  while(Serial.available()) {
+    Serial.read();
+}  
+}
